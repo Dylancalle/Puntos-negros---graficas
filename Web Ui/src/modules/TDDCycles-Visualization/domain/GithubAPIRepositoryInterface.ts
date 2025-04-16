05dcb7480d265a734683c004aede75b040bc4627
Dylan <dylan.calle@ucb.edu.bo>
2025-04-16 15:53:47 -0400

    Reinicio limpio del repo

---
+++ Web Ui/src/modules/TDDCycles-Visualization/domain/GithubAPIRepositoryInterface.ts
@@ -0,0 +1,15 @@
+import { CommitDataObject } from "./githubCommitInterfaces";
+
+export interface GithubAPIRepository {
+  obtainCommitsOfRepo(
+    owner: string,
+    repoName: string,
+  ): Promise<CommitDataObject[]>;
+
+  obtainRunsOfGithubActions(owner: string, repoName: string): any;
+
+  obtainJobsOfRepo(owner: string, repoName: string): any;
+  obtainComplexityOfRepo(owner:string, repoName: string): any;
+
+  obtainCommitTddCycle(owner:string, repoName: string): any;
+}
